7abf4a7ec21bc762e20d9a069901299d4b3e3396
Heyder Dias <heyderpd@gmail.com>
2016-09-11 19:10:37 -0300

    implement math

--- js/math-lab.js
+++ js/math-lab.js
@@ -1,15 +1,66 @@
 
+const objBit = require('./bit')
 const objBinary = require('./binary')
 
 main = () => {
   this.binaryLength = 0
 }
 
-main.convertObj = {}
+/*|||||||||||||||||||||||||||||||||||||||||*/
+/* CONVERT CONVERT CONVERT CONVERT CONVERT */
+/*|||||||||||||||||||||||||||||||||||||||||*/
 
-main.convertObj.Bin = (binary) => {
-  return new objBinary(binary)
+main.convert = {}
+
+main.convert.binaryArray = (decimalArray) => {
+  let binaryArray = []
+  this.doEach(decimalArray,
+    decimal => {
+      const binary = this.convert.toBinary(decimal)
+      decimal.binary.set(binary)
+      binaryArray.push(binary)
+    })
+  return binaryArray
 }
+
+main.convert.toBinary = (decimal) => {
+  const maxDecimal = decimal.maxDecimal()
+  const padding = this.calculate.padding(maxDecimal)
+  const binaryString = this.toBin(decimal.get(), padding)
+  const binary = this.convert.bitArray(binaryString)
+  return new objBinary(binary, padding, decimal)
+}
+
+main.convert.bitArray = (binaryString) => {
+  let bitArray = []
+  this.doEach(binaryString,
+    bit => {
+      const bitOjb = new objBit(bit)
+      bitArray.push(bitOjb)
+    })
+  return bitArray
+}
+
+main.convert.bit = (bitValue, padding) => {
+  if (bitValue !== 0 || bitValue !== 1) {
+    throw new Error("[fast-genetic] binary <> [1, 0]")
+  }
+  return new objBit(bitValue)
+}
+
+/*|||||||||||||||||||||||||||||||||||||||||*/
+/* CALCULATE CALCULATE CALCULATE CALCULATE */
+/*|||||||||||||||||||||||||||||||||||||||||*/
+
+main.calculate = {}
+
+main.calculate.padding = (maxDecimal) => {
+  return maxDecimal.toString(2).length
+}
+
+/*|||||||||||||||||||||||||||||||||||||||||*/
+/*  BASE    BASE    BASE    BASE    BASE   */
+/*|||||||||||||||||||||||||||||||||||||||||*/
 
 main.padding = (binary, padding) => {
   const defPadding = padding >= 0 ? padding : this.binaryLength

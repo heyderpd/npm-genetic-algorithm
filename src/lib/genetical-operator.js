be115af36debf564a40966ea1670315306f59f1f
Heyder Pestana Dias <heyder.dias@b2wdigital.com>
2016-12-13 18:55:18 -0200

    create shuffle

--- src/lib/genetical-operator.js
+++ src/lib/genetical-operator.js
@@ -1,7 +1,32 @@
 
 class operator {
   shuffle = (sampleA, sampleB) => {
-    return sampleA
+    return {
+      before: this.shufflePart(
+        sampleA.before,
+        sampleB.before),
+      after: this.shufflePart(
+        sampleA.after,
+        sampleB.after)
+    }
+  }
+
+  shufflePart = (sampleA, sampleB) => {
+    let minor, major
+    if (sampleA.length < sampleB.length) {
+      minor = sampleA
+      major = sampleB
+    } else {
+      minor = sampleB
+      major = sampleA
+    }
+    minor.map(
+      (char, i) => {
+        if (i % 2){
+          major[i] = char
+        }
+      })
+    return major
   }
 }
 
